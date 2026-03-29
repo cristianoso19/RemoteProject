5b2fac0db2bffb9ad816ea4bdd328c46697815df
Cristian Sacta <cristianoso19@gmail.com>
2026-03-28 20:34:39 -0500

    Refactor Python files: restore comments.py, add sintaxis.py, and create variables.py with examples

---
+++ Fundamentals/Python/3. variables/variables.py
@@ -0,0 +1,19 @@
+x = "This is a sring variable"
+print(x)
+
+x = "This is a new string variable"
+print(x)
+
+myvariable = "This is a variable"
+my_variable = "This is another variable"
+_my_variable = "This is yet another variable"
+myVariable = "This is a variable with camel case"
+MIVARIABLE = "This is a constant variable"
+
+2variable = "This is an invalid variable name because it starts with a number"
+my-variable = "This is an invalid variable name because it contains a hyphen"
+mi variable = "This is an invalid variable name because it contains a space"
+
+camelCase = "This is a variable with camel case"
+PascalCase = "This is a variable with Pascal case"
+snake_case = "This is a variable with snake case"
