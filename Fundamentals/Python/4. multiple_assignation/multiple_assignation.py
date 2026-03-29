196954508c0ad74e9aef929250158bbe7d48577f
Cristian Sacta <cristianoso19@gmail.com>
2026-03-28 20:42:07 -0500

    Add multiple assignment examples in Python

---
+++ Fundamentals/Python/4. multiple_assignation/multiple_assignation.py
@@ -0,0 +1,13 @@
+x,y,z = "Apple", "Banana", "Cherry"
+print(x,y,z)
+
+a = b = c = "Orange"
+print(a,b,c)
+
+print("My favorite fruit is " + a)
+print(a + " " + z)
+
+#concat with ,
+print("My favorite fruit is", a)
+#concat with f-string
+print(f"My favorite fruit is {a}")
