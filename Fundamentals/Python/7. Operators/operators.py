e358cd49a0ba43591ddf99473b5b62526a6f2769
Cristian Sacta <cristianoso19@gmail.com>
2026-03-30 22:44:49 -0500

    Add operators.py file with basic operators examples

---
+++ Fundamentals/Python/7. Operators/operators.py
@@ -0,0 +1,36 @@
+#Operators in Python
+#Arithmetic Operators
+x = 10
+y = 3
+
+#Sum
+print("Sum:", x + y) #Output: Sum: 13
+#Subtraction
+print("Subtraction:", x - y) #Output: Subtraction: 7
+#Multiplication
+print("Multiplication:", x * y) #Output: Multiplication: 30
+#Division
+print("Division:", x / y) #Output: Division: 3.333333333 (float division)
+#Module
+print("Module:", x % y) #Output: Module: 1 (remainder of the division)
+#Exponentiation
+print("Exponentiation:", x ** y) #Output: Exponentiation: 1000 (x raised to the power of y)
+#Division
+print("Floor Division:", x // y) #Output: Floor Division: 3 (integer division, discards the decimal part)
+
+#even or odd example
+number = 10
+if number % 2 == 0:
+    print("The number is even") #Output: The number is even
+else:
+    print("The number is odd") #Output: The number is odd
+    
+#Precedence of operators
+#order of precedence: 
+# parentheses, 
+# exponentiation, 
+# multiplication/division, 
+# addition/subtraction
+# comparision identity, membership
+#logical operators
+
