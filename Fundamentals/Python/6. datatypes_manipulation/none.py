dc0c1a7a68053b650cbde3a8be48bf5e93775711
Cristian Sacta <cristianoso19@gmail.com>
2026-03-30 22:36:48 -0500

    Add none.py with examples of None type and its usage in Python

---
+++ Fundamentals/Python/6. datatypes_manipulation/none.py
@@ -0,0 +1,10 @@
+#None is a special value in Python that represents the absence of a value or a null value
+x = None
+print(x) #Output: None
+print(type(x)) #Output: <class 'NoneType'>
+
+print(type(None)) #Output: <class 'NoneType'>
+print(type("")) #Output: <class 'str'>
+print(type(0)) #Output: <class 'int'>
+print(type([])) #Output: <class 'list'>
+print(type(False)) #Output: <class 'bool'>
